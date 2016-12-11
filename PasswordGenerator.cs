75b88b25e175f6fa1c78d5099ea1a81984f89fab
clmmbs <msreywel@hotmail.com>
2016-12-11 20:31:29 +0200

    Update and rename m.cs to PasswordGenerator.cs

--- PasswordGenerator.cs
+++ PasswordGenerator.cs
@@ -1,12 +1,12 @@
 using System;
-public class m
+public class PasswordGenerator
 {
 	static void Main()
 	{
 		string all="89plokm0ijnuhb7ygv6tfc5rdx4esz3waq12QWERTYUIOPLAKSJDHFGMZNXBCV[]{};:/?.,<>!@#$%^&*()-=_+", mypass="";
 		var rnd = new Random();
-		int pwlen=8;
-		for (int i=1; i<=pwlen; i++)
+		int pw_len=8;
+		for (int i=1; i<=pw_len; i++)
 			mypass += all[rnd.Next(all.Length)];
 		Console.WriteLine("Your password is:");
 		Console.Write(mypass);
